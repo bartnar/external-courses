dcbb3268418f6bdf088e2e92cbfa78acb6655e90
bartnar <bartnaruto@gmail.com>
2019-04-10 21:49:13 +0300

    Calculator with methods add, subtract, divide, multiply, getResult and reset added

--- src/ex5_js-functions/task.js
+++ src/ex5_js-functions/task.js
@@ -1,34 +1,36 @@
 var Calculator = {
-  answer: 0,
+  result: 0,
   add: function add(argument) {
     if (typeof argument === 'number' && !isNaN(argument)) {
-      Calculator.answer += argument;
+      Calculator.result += argument;
     }
     return add;
   },
   subtract: function subtract(argument) {
     if (typeof argument === 'number' && !isNaN(argument)) {
-      Calculator.answer -= argument;
+      Calculator.result -= argument;
     }
     return subtract;
-  } ,
+  },
   divide: function divide(argument) {
     if (typeof argument === 'number' && !isNaN(argument)) {
-      Calculator.answer /= argument;
+      Calculator.result /= argument;
     }
     return divide;
-  } ,
+  },
   multiply: function multiply(argument) {
     if (typeof argument === 'number' && !isNaN(argument)) {
-      Calculator.answer *= argument;
+      Calculator.result *= argument;
     }
     return multiply;
-  } ,
+  },
   getResult: function getResult() {
-    return Calculator.answer;
+    return Calculator.result;
   },
   reset: function reset() {
-    Calculator.answer = 0;
-    return reset;
+    Calculator.result = 0;
+    return this.getResult();
   }
 }
+module.exports = Calculator;
+
