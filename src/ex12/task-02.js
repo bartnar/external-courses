eb3ea72dc7116b8e39596b890a58c1351833516d
bartnar <bartnaruto@gmail.com>
2019-05-09 16:22:26 +0300

    now using modern way with addEventListener

--- src/ex12/task-02.js
+++ src/ex12/task-02.js
@@ -1,3 +1,5 @@
+topMenu.addEventListener("click", showHide);
+
 function showHide () {
 	var click = document.getElementById("drop-content");
 	if (click.style.display === "none") {
@@ -6,10 +8,3 @@ function showHide () {
 		click.style.display = "none";
 	}
 }
-
-
-/*linter said "no"*/
-/*var showHide = () => 
-{let click = document.getElementById("drop-content");
-click.style.display === "none" ? click.style.display = "block" : click.style.display = "none"};
-*/
