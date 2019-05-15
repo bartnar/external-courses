7aa318bec5ddf407b0c6fe4965ef2a51e4b91ca8
bartnar <bartnaruto@gmail.com>
2019-05-15 10:35:36 +0300

    calculator added

---
+++ src/ex14_js-context/task-01.js
@@ -0,0 +1,49 @@
+var Calculator = {
+  result: 0,
+  isNumber: function(value) {
+    return !isNaN(value) && typeof value === "number";
+  },
+  setState: function(argument) {
+    if (this.isNumber(argument)) {
+      this.result = argument;
+    }
+    return this;
+  },
+  fetchData: function(callback) {
+    setTimeout(function() {
+      callback(500);
+    }, 0);
+  },
+  add: function(argument) {
+    if (this.isNumber(argument)) {
+      this.result += argument;
+    }
+    return this;
+  },
+  subtract: function(argument) {
+    if (this.isNumber(argument)) {
+      this.result -= argument;
+    }
+    return this;
+  },
+  divide: function(argument) {
+    if (this.isNumber(argument)) {
+      this.result /= argument;
+    }
+    return this;
+  },
+  multiply: function(argument) {
+    if (this.isNumber(argument)) {
+      this.result *= argument;
+    }
+    return this;
+  },
+  getResult: function() {
+    return this.result;
+  },
+  reset: function() {
+    this.result = 0;
+    return this;
+  }
+};
+module.exports = Calculator;
